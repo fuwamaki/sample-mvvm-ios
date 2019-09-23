3b9c0761badc52f30945709d75e74407d82fd2cc
fuwamaki <yusaku.maki@street-academy.com>
2019-09-23 14:27:54 +0900

    create Project

---
+++ SampleMVVM/SampleMVVM/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/09/23.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
