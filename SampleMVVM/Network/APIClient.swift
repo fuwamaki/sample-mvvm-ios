012e4eaa43795a47671376f2d157c1c0c2d618fd
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 00:39:58 +0900

    add fetchItem in APIClient

--- SampleMVVM/Network/APIClient.swift
+++ SampleMVVM/Network/APIClient.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 import Alamofire
+import RxSwift
+import RxCocoa
 
 struct APIClient {
-
     func call<T: RequestProtocol>(request: T, completion: @escaping (OriginalResult<T.Response, Error>) -> Void) {
         Alamofire.request(request.url,
                           method: request.method,
@@ -33,5 +34,19 @@ struct APIClient {
                 case .failure(let error):
                     completion(.failure(error))
                 }}
+    }
+
+    func fetchItems() -> Single<[Item]> {
+        return Single<[Item]>.create(subscribe: { single in
+            self.call(request: ItemsFetchRequest()) { result in
+                switch result {
+                case .success(let response):
+                    single(.success(response.data))
+                case .failure(let error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        })
     }
 }
