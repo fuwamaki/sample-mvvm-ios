96ed2e21c4eb4e4ff3a027d38991576aba4f7e3f
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 16:04:46 +0900

    add TableCells

---
+++ SampleMVVM/Views/List/GithubTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  GithubTableCell.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/09/24.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+final class GithubTableCell: UITableViewCell {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ SampleMVVM/Views/List/QiitaTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  QiitaTableCell.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/09/24.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+final class QiitaTableCell: UITableViewCell {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
