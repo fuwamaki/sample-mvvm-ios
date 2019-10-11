1718750837e123e7484e9a994360f315df498f2c
fuwamaki <yusaku.maki@street-academy.com>
2019-10-11 18:51:32 +0900

    fix Item

--- SampleMVVM/Model/Item.swift
+++ SampleMVVM/Model/Item.swift
@@ -8,50 +8,21 @@
 
 import Foundation
 
-struct RequestItem: Codable {
-    let item: Item
-}
-
 struct Item: Codable {
     let id: Int?
     let name: String
     let category: String
     let price: Int
 
-    var requestData: Data {
+    // request data for post API
+    var postRequestData: Data {
+        struct RequestItem: Codable {
+            let item: Item
+        }
         let requestItem = RequestItem(item: self)
         guard let data = try? JSONEncoder().encode(requestItem) else {
             fatalError(MVVMError.jsonParseError.localizedDescription)
         }
         return data
-    }
-
-    // [String: Any]のDictionary型Model
-    var dictionary: [String: Any] {
-        guard let data = try? JSONEncoder().encode(self) else {
-            fatalError(MVVMError.jsonParseError.localizedDescription)
-        }
-        do {
-            let json = try JSONSerialization.jsonObject(with: data) as! [String: Any]
-            return json
-        } catch {
-            fatalError("json parse error")
-        }
-    }
-
-    var postRequestDictionary: [String: Any] {
-        struct PostRequest: Codable {
-            let item: Item
-        }
-        let request = PostRequest(item: self)
-        guard let data = try? JSONEncoder().encode(request) else {
-            fatalError(MVVMError.jsonParseError.localizedDescription)
-        }
-        do {
-            let json = try JSONSerialization.jsonObject(with: data) as! [String: Any]
-            return json
-        } catch {
-            fatalError("json parse error")
-        }
     }
 }
