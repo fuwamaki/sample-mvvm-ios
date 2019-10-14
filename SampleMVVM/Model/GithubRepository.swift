f1fbd0b05621e4fdfe150a910ebfb5d7710ec75e
fuwamaki <yusaku.maki@street-academy.com>
2019-10-14 12:43:57 +0900

    add GithubRepository Model

---
+++ SampleMVVM/Model/GithubRepository.swift
@@ -0,0 +1,30 @@
+//
+//  GithubRepository.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/10/14.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import Foundation
+
+struct GithubRepository: Codable {
+    let fullName: String
+    let stargazersCount: Int
+    let htmlUrl: String
+    let owner: GithubRepositoryOwner
+
+    enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+        case stargazersCount = "stargazers_count"
+        case htmlUrl = "html_url"
+        case owner
+    }
+
+    init(fullName: String, stargazersCount: Int, htmlUrl: String, owner: GithubRepositoryOwner) {
+        self.fullName = fullName
+        self.stargazersCount = stargazersCount
+        self.htmlUrl = htmlUrl
+        self.owner = owner
+    }
+}
