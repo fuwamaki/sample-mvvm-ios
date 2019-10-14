a0191065a89633bd6ab2039bb49d7a71725d9eb4
fuwamaki <yusaku.maki@street-academy.com>
2019-10-14 16:56:56 +0900

    fix GithubView

--- SampleMVVM/Views/List/GithubViewController.swift
+++ SampleMVVM/Views/List/GithubViewController.swift
@@ -22,12 +22,25 @@ final class GithubViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView! {
         didSet {
-//            tableView.register(R.nib.)
-//            tableView.registerForCell(GithubTableCell.self)
-//            tableView.delegate = self
-//            tableView.dataSource = self
-            tableView.tableFooterView = UIView()
-            tableView.backgroundColor = UIColor.white
+            tableView.register(R.nib.githubTableCell)
+
+            tableView.rx
+                .setDelegate(self)
+                .disposed(by: disposeBag)
+
+            viewModel.repositories
+                .drive(tableView.rx.items) { tableView, index, element in
+                    let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.githubTableCell,
+                                                             for: IndexPath(item: index, section: 0))!
+                    cell.render(repository: element)
+                    return cell }
+                .disposed(by: disposeBag)
+
+            tableView.rx.itemSelected
+                .subscribe(onNext: { [unowned self] indexPath in
+                    self.tableView.deselectRow(at: indexPath, animated: false)
+                    self.viewModel.showGithubWebView(indexPath: indexPath)})
+                .disposed(by: disposeBag)
         }
     }
 
@@ -41,11 +54,11 @@ final class GithubViewController: UIViewController {
         return indicator
     }()
 
-    private var isHideIndicator: Bool = true {
+    private var isLoading: Bool = false {
         didSet {
             DispatchQueue.main.async {
-                self.isHideIndicator ? self.indicator.stopAnimating() : self.indicator.startAnimating()
-                self.indicator.isHidden = self.isHideIndicator
+                self.isLoading ? self.indicator.startAnimating() : self.indicator.stopAnimating()
+                self.indicator.isHidden = !self.isLoading
             }
         }
     }
@@ -55,18 +68,40 @@ final class GithubViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
         bind()
     }
 
     private func setupTableView() {
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = UIColor.white
     }
 
     private func bind() {
+        viewModel.isLoading
+            .subscribe(onNext: { [unowned self] in
+                self.isLoading = $0
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.pushViewController
+            .drive(onNext: { [unowned self] viewController in
+                self.navigationController?
+                    .pushViewController(viewController, animated: true)})
+            .disposed(by: disposeBag)
+
+        searchButton.rx.tap
+            .subscribe(onNext: { [unowned self] in
+                self.viewModel.fetchRepositories(query: self.searchBar.text)
+                    .subscribe()
+                    .disposed(by: self.disposeBag)
+            }).disposed(by: disposeBag)
     }
 }
 
 extension GithubViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
         return GithubTableCell.defaultHeight(tableView)
     }
 }
--- SampleMVVM/Views/List/GithubViewModel.swift
+++ SampleMVVM/Views/List/GithubViewModel.swift
@@ -8,8 +8,15 @@
 
 import RxSwift
 import RxCocoa
+import SafariServices
 
 protocol GithubViewModelable {
+    var isLoading: BehaviorRelay<Bool> { get }
+    var repositories: Driver<[GithubRepository]> { get }
+    var presentViewController: Driver<UIViewController> { get }
+    var pushViewController: Driver<UIViewController> { get }
+    func fetchRepositories(query: String?) -> Completable
+    func showGithubWebView(indexPath: IndexPath)
 }
 
 final class GithubViewModel {
@@ -27,22 +34,36 @@ final class GithubViewModel {
     var presentViewController: Driver<UIViewController> {
         return presentViewControllerSubject.asDriver(onErrorJustReturn: UIViewController())
     }
+
+    private var pushViewControllerSubject = PublishRelay<UIViewController>()
+    var pushViewController: Driver<UIViewController> {
+        return pushViewControllerSubject.asDriver(onErrorJustReturn: UIViewController())
+    }
 }
 
 extension GithubViewModel: GithubViewModelable {
-    func fetchRepositories(query: String) -> Completable {
+    func fetchRepositories(query: String?) -> Completable {
+        guard let query = query else {
+            return Completable.empty()
+        }
         isLoading.accept(true)
         return apiClient.fetchGithubRepositories(query: query)
-        .do(
-            onSuccess: { [weak self] repositories in
-                self?.isLoading.accept(false)
-                self?.repositoriesSubject.accept(repositories)
-            },
-            onError: { [weak self] error in
-                self?.isLoading.accept(false)
-                self?.presentViewControllerSubject
-                    .accept(UIAlertController.singleErrorAlert(message: error.localizedDescription))})
-        .map { _ in } // Single<Void>に変換
-        .asCompletable() // Completableに変換
+            .do(
+                onSuccess: { [weak self] repositories in
+                    self?.isLoading.accept(false)
+                    self?.repositoriesSubject.accept(repositories)
+                },
+                onError: { [weak self] error in
+                    self?.isLoading.accept(false)
+                    self?.presentViewControllerSubject
+                        .accept(UIAlertController.singleErrorAlert(message: error.localizedDescription))})
+            .map { _ in } // Single<Void>に変換
+            .asCompletable() // Completableに変換
+    }
+
+    func showGithubWebView(indexPath: IndexPath) {
+        guard let url = URL(string: repositoriesSubject.value[indexPath.row].htmlUrl) else { return }
+        let viewController = SFSafariViewController(url: url)
+        pushViewControllerSubject.accept(viewController)
     }
 }
