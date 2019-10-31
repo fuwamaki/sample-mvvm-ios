e2447bb91ec0505a66ed1a72c8406cab29c3b695
fuwamaki <yusaku.maki@street-academy.com>
2019-11-01 01:24:18 +0900

    display pickerView on MypageView

--- SampleMVVM/Views/Mypage/MypageViewController.swift
+++ SampleMVVM/Views/Mypage/MypageViewController.swift
@@ -12,14 +12,38 @@ import RxCocoa
 
 final class MypageViewController: UIViewController {
 
+    @IBOutlet weak var textTextField: UITextField!
+
     private let disposeBag = DisposeBag()
     private let viewModel: MypageViewModelable = MypageViewModel()
 
+    private let pickerView = UIPickerView()
+    private let picker = CustomPicker()
+    private let array = ["1", "2", "3", "4", "5"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
         bind()
     }
 
+    private func setupViews() {
+        picker.array = array
+        pickerView.delegate = picker
+        pickerView.dataSource = picker
+        textTextField.inputView = pickerView
+        let inputAccessoryView = TextFieldInputAccessoryView(textField: textTextField)
+        inputAccessoryView.delegate = self
+        textTextField.inputAccessoryView = inputAccessoryView
+    }
+
     private func bind() {
+        picker.selectedContent
+            .subscribe(onNext: { [unowned self] text in
+                self.textTextField.text = text
+            })
+            .disposed(by: disposeBag)
     }
 }
+
+extension MypageViewController: TextFieldInputAccessoryViewDelegate {}
