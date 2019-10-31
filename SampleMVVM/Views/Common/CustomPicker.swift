83375f4e80f6f532c2381deced5e69bd425308fa
fuwamaki <yusaku.maki@street-academy.com>
2019-11-01 01:24:07 +0900

    add CustomPicker

---
+++ SampleMVVM/Views/Common/CustomPicker.swift
@@ -0,0 +1,36 @@
+//
+//  CustomPicker.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/10/31.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class CustomPicker: NSObject {
+    var array: [String] = []
+    var selectedContent = BehaviorRelay<String?>(value: nil)
+}
+
+extension CustomPicker: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return array.count
+    }
+}
+
+extension CustomPicker: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return array[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedContent.accept(array[row])
+    }
+}
