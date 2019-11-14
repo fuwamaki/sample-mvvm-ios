ff03c0f4f62ce781da80928e4bd9aba4b1057618
fuwamaki <yusaku.maki@street-academy.com>
2019-11-15 00:10:22 +0900

    fix QiitaItemResponse

--- SampleMVVM/Network/Response/QiitaItemResponse.swift
+++ SampleMVVM/Network/Response/QiitaItemResponse.swift
@@ -11,3 +11,20 @@ import Foundation
 struct QiitaItemFetchResponse: ResponseProtocol {
     let items: [QiitaItem]
 }
+
+extension QiitaItemFetchResponse: Decodable {
+    init(from decoder: Decoder) throws {
+        var items: [QiitaItem] = []
+        var unkeyedContainer = try decoder.unkeyedContainer()
+        while !unkeyedContainer.isAtEnd {
+            let item = try unkeyedContainer.decode(QiitaItem.self)
+            items.append(item)
+        }
+        self.init(items: items)
+    }
+}
+
+// Rootが配列型のJSONをCodableでマッピングしてくれる魔法のprotocol
+private protocol Decodable {
+    init(from decoder: Decoder) throws
+}
