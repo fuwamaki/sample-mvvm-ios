bd10963cc9ecabb76ad90468d5f819d388b00f74
fuwamaki <yusaku.maki@street-academy.com>
2019-11-15 00:03:20 +0900

    add QiitaURL

--- SampleMVVM/Network/API/APIClientable.swift
+++ SampleMVVM/Network/API/APIClientable.swift
@@ -10,11 +10,13 @@ import RxSwift
 import RxCocoa
 
 protocol APIClientable {
-    // MARK: - Item
+    // MARK: Item
     func fetchItems() -> Single<[Item]>
     func postItem(item: Item) -> Completable
     func deleteItem(id: Int) -> Completable
     func putItem(id: Int, item: Item) -> Completable
-    // MARK: - Github
+    // MARK: Github
     func fetchGithubRepositories(query: String) -> Single<[GithubRepository]>
+    // MARK: Qiita
+    func fetchQiitaItems(tag: String) -> Single<[QiitaItem]>
 }
