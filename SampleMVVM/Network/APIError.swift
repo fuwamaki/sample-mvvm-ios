82f102ffe7b6291e4a626b9e03191fe57a20b432
fuwamaki <yusaku.maki@street-academy.com>
2019-11-14 22:51:38 +0900

    fix APIError

--- SampleMVVM/Network/APIError.swift
+++ SampleMVVM/Network/APIError.swift
@@ -7,13 +7,27 @@
 //
 
 enum APIError: Error {
+    case customError(message: String)
+    case nonDataError // 200なのにResponseがnilの場合
+    case unauthorizedError // 401
+    case notFoundError // 404
+    case maintenanceError // 503
     case networkError
     case jsonParseError
-    case invalidRequest
-    case serverError
+    case unknownError
 
     var message: String {
         switch self {
+        case .customError(let message):
+            return message
+        case .nonDataError:
+            return "申し訳ありません、データがありませんでした。"
+        case .unauthorizedError:
+            return "ユーザーセッションの有効期限が切れたため、再度ログインしてください。"
+        case .notFoundError:
+            return "申し訳ありません、データが見つかりませんでした。"
+        case .maintenanceError:
+            return "メンテナンス中です。終了までしばらくお待ちください。"
         case .networkError:
             return "通信エラーが発生しました。電波の良い所で再度お試しください。"
         default:
