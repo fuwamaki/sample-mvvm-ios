b3df1b6f279a776ca2a0f1fecd0f645bb634a084
fuwamaki <yusaku.maki@street-academy.com>
2019-11-15 11:40:30 +0900

    add UserDefaults Extension files

---
+++ SampleMVVM/Extension/UserDefaults+BoolDefaultSettable.swift
@@ -0,0 +1,34 @@
+//
+//  UserDefaults+BoolDefaultSettable.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/11/15.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+protocol BoolDefaultSettable: KeyNamespaceable {
+    associatedtype BoolKey: RawRepresentable
+}
+
+extension BoolDefaultSettable where BoolKey.RawValue == String {
+    func set(_ value: Bool, forKey key: BoolKey) {
+        let key = namespaced(key)
+        UserDefaults.standard.set(value, forKey: key)
+    }
+
+    @discardableResult
+    func bool(forKey key: BoolKey) -> Bool? {
+        let key = namespaced(key)
+        return UserDefaults.standard.bool(forKey: key)
+    }
+}
+
+extension UserDefaults: BoolDefaultSettable {
+    enum BoolKey: String {
+        case isTutorial
+        case isLogin
+        case canUserNotification
+    }
+}
---
+++ SampleMVVM/Extension/UserDefaults+StringDefaultSettable.swift
@@ -0,0 +1,35 @@
+//
+//  UserDefaults+StringDefaultSettable.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/11/15.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+protocol StringDefaultSettable: KeyNamespaceable {
+    associatedtype StringKey: RawRepresentable
+}
+
+extension StringDefaultSettable where StringKey.RawValue == String {
+    func set(_ value: String, forKey key: StringKey) {
+        let key = namespaced(key)
+        UserDefaults.standard.set(value, forKey: key)
+    }
+
+    @discardableResult
+    func string(forKey key: StringKey) -> String? {
+        let key = namespaced(key)
+        return UserDefaults.standard.string(forKey: key)
+    }
+}
+
+extension UserDefaults: StringDefaultSettable {
+    enum StringKey: String {
+        case mode
+        case userId
+        case userName
+        case userImageUrl
+    }
+}
