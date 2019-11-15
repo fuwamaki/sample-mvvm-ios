4a96b7f1417fe2f717f72e84092de273a08f1874
fuwamaki <yusaku.maki@street-academy.com>
2019-11-15 12:53:53 +0900

    add ListRealmEntity

---
+++ SampleMVVM/Entity/ListRealmEntity.swift
@@ -0,0 +1,16 @@
+//
+//  ListRealmEntity.swift
+//  SampleMVVM
+//
+//  Created by yusaku maki on 2019/11/15.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ListRealmEntity: Object, RealmModelable {
+    @objc dynamic var itemId: Int = 0
+    @objc dynamic var keyword: String = ""
+    @objc dynamic var type: String = ""
+}
