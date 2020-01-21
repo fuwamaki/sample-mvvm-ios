ea58788853a3292af599a8b79651691cabb3de41
fuwamaki <yusaku.maki@street-academy.com>
2020-01-21 17:24:05 +0900

    refactoring SceneDelegate

--- SampleMVVM/AppDelegate/SceneDelegate.swift
+++ SampleMVVM/AppDelegate/SceneDelegate.swift
@@ -13,10 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    /// アプリ起動時の初期Windowの設定
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard (scene as? UIWindowScene) != nil else { return }
     }
 
@@ -24,31 +22,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         _ = LoginManager.shared.application(.shared, open: URLContexts.first?.url)
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
+    /// アプリ起動時に呼ばれる。バックグランドからの再起動時にも呼ばれる。
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
     }
 
+    /// アプリ起動完了後に呼ばれる。バックグランドからの再起動時にも呼ばれる。
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    /// バックグラウンドへのスワイプ開始直後に呼ばれる。
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    /// バックグラウンド完了後に呼ばれる。
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+    }
+
+    /// タスクキル完了後に呼ばれる。
+    func sceneDidDisconnect(_ scene: UIScene) {
     }
 }
