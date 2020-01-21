91c0d9abc935df129bf1b88e2bdcaaccd98a9365
fuwamaki <yusaku.maki@street-academy.com>
2020-01-21 17:33:39 +0900

    refactoring AppDelegate

--- SampleMVVM/AppDelegate/AppDelegate.swift
+++ SampleMVVM/AppDelegate/AppDelegate.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import LineSDK
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         LoginManager.shared.setup(channelID: Constants.lineChannelID, universalLinkURL: nil)
+        SampleUserNotificationCenter.shared.setupPushNotification()
         return true
     }
 
@@ -25,18 +27,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
-    // MARK: UISceneSession Lifecycle
-
+    /// SceneDelegateの設定・指定: Info.plist > Application Scene Manifest を参照する
+    /// 処理として呼び出されるfunctionではない
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    /// タスクキルした瞬間の処理
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
